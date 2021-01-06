d27453ac796a81324675cd3756663ae3d836f806
Wojtek <wojciech3.zajac@student.uj.edu.pl>
2021-01-06 16:48:00 +0100

    rhdh

---
+++ Selects.sql
@@ -0,0 +1,99 @@
+USE Project
+SELECT * 
+FROM Players
+SELECT * 
+FROM Guilds
+SELECT * 
+FROM LocationsConnetions
+SELECT * 
+FROM Characters
+SELECT * 
+FROM Inventory
+SELECT * 
+FROM Banned
+SELECT * 
+FROM NPCs
+SELECT * 
+FROM Enemies
+SELECT * 
+FROM EnemyDrops
+SELECT * 
+FROM Friends
+SELECT * 
+FROM Stores
+SELECT * 
+FROM AuctionHouse
+SELECT * 
+FROM AuctionHouseBids
+SELECT * 
+FROM Quests
+SELECT * 
+FROM QuestsTracker
+SELECT * 
+FROM Rewards
+
+SELECT *
+FROM CurrentlyBanned
+
+SELECT dbo.TryToLogin ('email1@wp.pl', 'password 123')
+SELECT dbo.TryToLogin ('email2@wp.pl', 'password 123')
+SELECT dbo.TryToLogin ('email3@wp.pl', 'password 123')
+SELECT dbo.TryToLogin ('email2@wp.pl', 'password 1234')
+SELECT dbo.TryToLogin ('sxggs', 'fe52hhd')
+
+
+SELECT *
+FROM CharacterInventory (1)
+SELECT *
+FROM CharacterInventory (2)
+SELECT *
+FROM CharacterInventory (3)
+SELECT *
+FROM CharacterInventory (4)
+
+SELECT *
+FROM PlayerCharacters(1)
+SELECT *
+FROM PlayerCharacters(2)
+/*
+SELECT *
+FROM CharactersInGuild
+SELECT *
+FROM EnemiesInLocation
+SELECT *
+FROM FriendsInLocation
+SELECT *
+FROM AccessibleLocations
+SELECT *
+FROM NPCsQuests
+SELECT *
+FROM AccessibleQuests
+SELECT *
+FROM ItemsInStore
+SELECT *
+FROM RewardsForQuestGO
+SELECT *
+ 
+
+ 
+
+  */
+
+ 
+
+   
+
+  
+
+  
+
+   
+
+  
+
+   
+
+ 
+
+ 
+ 
